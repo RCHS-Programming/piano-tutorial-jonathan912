751b82e7b2c8f4b1b423f522b2bdaba548d158a7
jonathan <80464146+jonathan912@users.noreply.github.com>
2021-04-19 08:38:15 -0400

    added different keys
    added where you could add different keys without copying and pasting over and over again

--- Key.java
+++ Key.java
@@ -1,18 +1,31 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
-
-public class Key extends Actor
-{
+public class Key extends Actor{
+    private boolean isDown;
+    private String key;
+    private String sound;
     /**
      * Create a new key.
      */
-    public Key()
+    public Key(String keyName, String soundFile)
     {
+        key = keyName;
+        sound= soundFile;
     }
-
     /**
      * Do the action for this key.
      */
     public void act()
     {
+        if( !isDown && Greenfoot.isKeyDown(key))
+            {
+            Greenfoot.playSound(sound);
+            setImage("white-key-down.png");
+            isDown= true;
+            }
+        if( isDown && !Greenfoot.isKeyDown(key))
+            {
+                setImage("white-key.png");
+                isDown = false; 
+            }
     }
 }
